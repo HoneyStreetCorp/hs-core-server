a9b906814bf6cd390ba786a911e6b82870a89be3
hoo47 <128340192+hoo47@users.noreply.github.com>
2023-05-08 21:46:55 +0900

    Feat: add quetions schema inform

--- innit_db.sql
+++ innit_db.sql
@@ -2,3 +2,11 @@ create table users(
     user_id varchar(36) primary key,
     created_at datetime not null
 );
+
+create table questions(
+    question_id varchar(36) primary key,
+    title varchar(255) not null,
+    body varchar(255) null,
+    answer JSON,
+    created_at datetime not null
+);

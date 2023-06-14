30c1e77c5753aa5631faca698511507359146307
hoo47 <128340192+hoo47@users.noreply.github.com>
2023-06-14 22:12:54 +0900

    Feat: add import for session

--- application/user_create_service.py
+++ application/user_create_service.py
@@ -1,8 +1,10 @@
+from sqlalchemy.orm import Session
+
 from model.user import User
 
 
 class UserCreateService:
-    def create_user(self, db) -> User:
+    def create_user(self, db: Session) -> User:
         user = User()
         db.add(user)
         db.commit()

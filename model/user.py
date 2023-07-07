675c8cfbf42b3508c4108870705e88cd1b68c451
hoo47 <128340192+hoo47@users.noreply.github.com>
2023-07-07 20:04:05 +0900

    Feat: add answer table

--- model/user.py
+++ model/user.py
@@ -13,4 +13,5 @@ def generate_uuid():
 class User(Base):
     __tablename__ = 'users'
     user_id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=generate_uuid)
+    name = Column(str, nullable=False)
     created_at = Column(DateTime, default=func.utc_timestamp())

65e18a42e7af7e417db9731b16231742e51067a9
hoo47 <128340192+hoo47@users.noreply.github.com>
2023-07-10 00:21:45 +0900

    Feat: add error handler

--- controller/user_create.py
+++ controller/user_create.py
@@ -1,9 +1,11 @@
-from typing import Any, Optional
+from typing import Any
 
-from fastapi import APIRouter, Depends, Query, HTTPException
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 
 from application import user_create_service
+from core.error import CustomException
+from dto.error.error_response import ErrorResponse
 from dto.response.user_response import UserResponse
 from middleware.deps import get_db
 
@@ -16,9 +18,6 @@ def create_user(
         name: str = Query(default=None)
 ) -> Any:
     if name is None:
-        raise HTTPException(
-            status_code=400,
-            detail="name requried"
-        )
+        raise CustomException(ErrorResponse(message="[ERROR] name field ommitted. Name required.", status_code=400))
     user = user_create_service.create_user(db, name)
     return UserResponse(id=user.user_id, name=user.name)
